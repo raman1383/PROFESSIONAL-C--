cb8ec40cfc295cc23c7e4393dda1f4b33bb3806d
raman1383 <raman83shariati@gmail.com>
2022-07-02 21:44:43 +0430

    r

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+// #include <iostream>
 #include "chapter_1/crashCourse.hh"
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	// std::cout << "Hello, World!" << std::endl;
 	forFun();
 
 	return 0;
