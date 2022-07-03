6cf9fc6e891addb797d96dc8fa50b26a7b351a65
raman1383 <raman83shariati@gmail.com>
2022-07-03 17:45:47 +0430

    R

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,10 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
+#include <vector>
+#include <ranges>
+#include <numbers>
+
 int main()
 {
-	auto i{0x01B99644};
-	std::string x{" DFaeeillnor"};
-	while (i--)
-		std::next_permutation(x.begin(), x.end());
-	std::cout << x;
+	auto x = std::numbers::pi;
+	std::cout << x << std::endl;
 }
