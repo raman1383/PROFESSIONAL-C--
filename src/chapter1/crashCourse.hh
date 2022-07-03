8a490664e283636e492f174737ce79ad950e6460
raman1383 <raman83shariati@gmail.com>
2022-07-03 18:25:54 +0430

    r

--- src/chapter1/crashCourse.hh
+++ src/chapter1/crashCourse.hh
@@ -5,16 +5,24 @@ using std::cout, std::cin, std::endl, std::string;
 namespace first
 {
     int firstX{1};
+
     namespace second
     {
         int secondX{first::firstX + 1};
+
+        namespace third
+        {
+            int thirdX{first::second::secondX + 1};
+        } // third
+
     } // namespace second
 
 } // namespace first
 
 namespace fs = first::second;
+namespace fst = first::second::third;
 
 void funFunction()
 {
-    cout << fs::secondX << endl;
+    cout << first::firstX << fs::secondX << fst::thirdX << endl; // 123
 }
